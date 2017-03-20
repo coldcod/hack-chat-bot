b004dd41e0b0db72282de21b3d3c38a00b5ac4a9
satwinderpal <satwindersapra@ymail.com>
2017-03-20 15:06:31 +0530

    .

--- ws-test.js
+++ ws-test.js
@@ -1,9 +1,10 @@
 const ws = require("ws")
 const wss = new ws("wss://hack.chat/chat-ws");
-wss.on('open', function open() {
-	try {
-    	wss.sendRaw({cmd: "join", nick: "mozFromWebSocket", channel: "?programming" });
-	} catch (e) {
-		console.log(e);
-	}
+wss.on('open', function() {
+		wss.send(JSON.stringify({cmd: "join", nick: "mozFromWebSocket#ldcod", channel: "programming" }));
+		var stdin = process.openStdin();
+		stdin.addListener("data", function(d) {
+		    var message = d.toString().trim();
+		    wss.send(JSON.stringify({cmd: "chat", text: message}));
+		});
 });
