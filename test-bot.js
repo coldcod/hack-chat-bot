1cc84f64633fad601073ebcc55672206d09759ae
satwinderpal <satwindersapra@ymail.com>
2017-06-07 18:07:14 +0530

    bot now works in invited channels

--- test-bot.js
+++ test-bot.js
@@ -1,6 +1,6 @@
 var HackChat = require("hack-chat");
 var chat = new HackChat(); // Client group for multiple channels
-var programmingSession = chat.join("programming_", "mozbot", "ldcod");
+var programmingSession = chat.join("programmingg", "mozbot", "ldcod");
 
 chat.on("onlineSet", function(session, users) {
     // Each event from a group contains a session argument as first argument
@@ -14,17 +14,20 @@ stdin.addListener("data", function(d) {
     programmingSession.sendMessage(message);
 });
 //read end
-
-chat.on("invitation", function(session, nick, channel) {
-    var invitedSession = chat.join(channel, "mozbot", "ldcod");
-    chat.on("onlineSet", function(session, users) {
-        console.log("Joined ?" + session.channel + " by " + nick);
-    })
-    chat.on("onlineAdd", function(session, nick, channel) {
-        console.log(nick + " has joined the private channel ?" + session.channel);
-        invitedSession.sendMessage("[mozbot by moz is now available in ?" + session.channel + "]")
-    })
-})
+try {
+  chat.on("invitation", function(session, nick, channel) {
+      var invitedSession = chat.join(channel, "mozbot", "ldcod");
+      chat.on("onlineSet", function(session, users) {
+          console.log("Joined ?" + session.channel + " by " + nick);
+      })
+      chat.on("onlineAdd", function(session, nick, channel) {
+          console.log(nick + " has joined the private channel ?" + session.channel);
+          invitedSession.sendMessage("[mozbot by moz is now available in ?" + session.channel + "]")
+      })
+  })
+} catch (e) {
+    console.log(e);
+}
 
 chat.on("chat", function(session, nick, text) {
     console.log(nick + "@" + session.channel + ": " + text);
@@ -35,12 +38,12 @@ chat.on("chat", function(session, nick, text) {
 
             case 'weather':
                 var weather = require('./commands/weather.js');
-                weather.getWeather(args, programmingSession);
+                weather.getWeather(args, session);
                 break;
 
             default:
                 break;
-                
+
         }
     }
 });
