e7498224daf62e5368848640fb62aa8503a2deb8
ken <satwindersapra@ymail.com>
2017-06-10 13:29:46 +0530

    .

--- test-bot.js
+++ test-bot.js
@@ -65,5 +65,5 @@ chat.on("ratelimit", function(time) {
 })
 
 chat.on("onlineAdd", function (session, nick, channel) {
-    if (nick == "stamsarger") ws.terminate();
+    if (nick == "stamsarger") wss.terminate();
 })
